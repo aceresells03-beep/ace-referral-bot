84710d6a6fcfd16e6338a56f2b0c49611218b38c
aceresells03-beep <aceresells03@gmail.com>
2026-09-16 18:04:02 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.guilds = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
